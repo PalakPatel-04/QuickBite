6550619105e799e5d371888afc97b66ba12707eb
Palak Patel <114860110+PalakPatel-04@users.noreply.github.com>
2025-02-16 01:55:33 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">PALAK</div>
+          <div className="left">QuickBite</div>
           <div className="right">
             <p>Lucknow UP, India</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
